e8e2a566109fe2cf284c9cee740c0bf2dca88c93
Adarsh Choudhary <68109483+CodeWithMax@users.noreply.github.com>
2022-03-29 23:20:19 +0530

    user added

---
+++ index.js
@@ -0,0 +1,109 @@
+[
+  {
+    userId: 1,
+    id: "1",
+    name: "Adarsh Choudhary",
+    email: "adarshchoudhary312@gmail.com",
+    website: "https://www.google.com",
+    mobile: "1234567899",
+    image:
+      "https://avatars.githubusercontent.com/u/68109483?s=400&u=3020a4a6f48bea955ef4c58083341af4137807fa&v=4",
+    description:
+      "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+  },
+  {
+    userId: 2,
+    id: "2",
+    name: "harsh choudhary",
+    email: "harsh@gmail.com",
+    website: "https://www.google.com",
+    image:
+      "https://images.unsplash.com/photo-1603415526960-f7e0328c63b1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    description:
+      "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+  },
+  {
+    userId: 3,
+    id: "3",
+    name: "random choudhary",
+    email: "random@gmail.com",
+    website: "https://www.google.com",
+    mobile: "1234567899",
+    image:
+      "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    description:
+      "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut",
+  },
+  {
+    userId: 4,
+    id: "4",
+    name: "saksham choudhary",
+    email: "saksham@gmail.com",
+    website: "https://www.google.com",
+    mobile: "1234567899",
+    image:
+      "https://images.unsplash.com/photo-1544168190-79c17527004f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=688&q=80",
+    description:
+      "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit",
+  },
+  {
+    userId: 5,
+    id: "5",
+    name: "manish singh",
+    email: "manish@gmail.com",
+    website: "https://www.google.com",
+    mobile: "1234567899",
+    image:
+      "https://images.unsplash.com/photo-1639149888905-fb39731f2e6c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80",
+    description:
+      "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque",
+  },
+  {
+    userId: 6,
+    id: "6",
+    name: "razer",
+    email: "razer@gmail.com",
+    website: "https://www.google.com",
+    mobile: "1234567899",
+    image:
+      "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    description:
+      "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae",
+  },
+  {
+    userId: 7,
+    id: "7",
+    name: "rahul",
+    email: "rahul@gmail.com",
+    website: "https://www.google.coml",
+    mobile: "1234567899",
+    image:
+      "https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    description:
+      "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas",
+  },
+  {
+    userId: 8,
+    id: "8",
+    name: "abhishek",
+    email: "abhishek@gmail.com",
+    website: "https://www.google.com",
+    mobile: "1234567899",
+    image:
+      "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    description:
+      "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae",
+  },
+  {
+    userId: 9,
+    id: "9",
+    name: "Harmendra",
+    email: "harmendra@gmail.com",
+    website: "https://www.youtube.com/thapatechnical",
+    mobile: "1234567899",
+    image:
+      "https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    description:
+      "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas",
+  },
+];
